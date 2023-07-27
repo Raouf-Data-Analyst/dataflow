f6ea178f8118707510bf92210261b51d2e7f17fa
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-07-27 16:01:32 +0200

    Update session-uprocs-table_dep.py

--- session-uprocs-table_dep.py
+++ session-uprocs-table_dep.py
@@ -3508,7 +3508,7 @@ def plot_network_graph():
         HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
     # Load HTML file in HTML component for display on Streamlit page
-    components.html(HtmlFile.read(), height=435)
+    st.components.html(HtmlFile.read(), height=435)
 
 
 
