bd3779e48bf9672268e09746bc68c18e0a136c8a
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-07-31 16:46:30 +0200

    Update session-uprocs-table_dep_V2.py

--- session-uprocs-table_dep_V2.py
+++ session-uprocs-table_dep_V2.py
@@ -83,6 +83,7 @@ def plot_network_graph():
     # Check if the node has "table_deps" and update the color accordingly
         if node_id in [table_dep["id"] for table_dep in nodes if table_dep["color"] == table_deps_color]:
             node_attributes["color"] = table_deps_color
+            node_attributes["label"] = node_id
         nt.add_node(node_id, **node_attributes)
 
 
