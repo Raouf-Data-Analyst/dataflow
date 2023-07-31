1fa6df63c0f9c9ac777cd3055ab29788f7c20d66
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-07-31 17:33:22 +0200

    Update Graphiz_test.py

--- Graphiz_test.py
+++ Graphiz_test.py
@@ -1709,9 +1709,35 @@ data = """
 json_data = json.loads(data)
 nodes = []
 edges = []
-# ... The rest of your code ...
+    # Function to add nodes and edges for inputs and outputs
+    def add_input_output_nodes(process_name, inputs, outputs):
+        for inp in inputs:
+            nodes.append({"id": inp, "title": "", "color": ""})
+            edges.append((inp, process_name))
+        for out in outputs:
+            nodes.append({"id": out, "title": "", "color": ""})
+            edges.append((process_name, out))
 
-# Your function to create the graph here
+    # Function to add nodes for table_deps
+    def add_table_deps_nodes(uprocs_name, table_deps):
+        for table_dep, table_dep_content in table_deps.items():
+            if table_dep not in nodes:
+                nodes.append({"id": table_dep, "title": table_dep_content, "color": "#00FFFF"}) 
+            edges.append((uprocs_name, table_dep))
+
+    # Process each process (uprocs) in the JSON data
+    for process_name, process_info in json_data.items():
+        nodes.append({"id": process_name, "title": "", "color": ""})
+        inputs = process_info.get("inputs", [])
+        outputs = process_info.get("outputs", [])
+        add_input_output_nodes(process_name, inputs, outputs)
+
+        for uprocs_info in process_info.get("uprocs", []):
+            uprocs_name = uprocs_info.get("name")
+            uprocs_label = uprocs_info.get("label")
+            nodes.append({"id": uprocs_name, "title": "", "color": ""})
+            edges.append((process_name, uprocs_name))
+            add_table_deps_nodes(uprocs_name, uprocs_info.get("table_deps", {}))
 
 # Create the function to visualize the graph on Streamlit
 def plot_network_graph():
