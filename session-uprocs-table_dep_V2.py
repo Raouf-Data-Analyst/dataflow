a3ef2012dd790da79a8a4aebcf7f2cd16abdcd2d
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-07-31 16:49:48 +0200

    Update session-uprocs-table_dep_V2.py

--- session-uprocs-table_dep_V2.py
+++ session-uprocs-table_dep_V2.py
@@ -84,7 +84,7 @@ def plot_network_graph():
         if node_id in [table_dep["id"] for table_dep in nodes if table_dep["color"] == table_deps_color]:
             node_attributes["color"] = table_deps_color
             node_attributes["label"] = node_id
-        nt.add_node(node_id, **node_attributes, label = node_id)
+            nt.add_node(node_id, **node_attributes, label = node_id)
 
 
 
