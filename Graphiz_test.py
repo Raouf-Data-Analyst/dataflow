8b27ade1f058cd02d582b372e1dfcb5deb6a309e
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-07-31 17:41:54 +0200

    Update Graphiz_test.py

--- Graphiz_test.py
+++ Graphiz_test.py
@@ -1,50 +1,50 @@
+import json
 import streamlit as st
-import graphviz as gv
+import pygraphviz as pgv
+import streamlit.components.v1 as components
 
-# Sample data for sessions, uprocs, and dependencies
-data = {
-    "sessions": ["Session1", "Session2", "Session3"],
-    "uprocs": ["Uproc1", "Uproc2", "Uproc3", "Uproc4"],
-    "dependencies": [
-        ("Session1", "Uproc1"),
-        ("Session1", "Uproc2"),
-        ("Session2", "Uproc2"),
-        ("Uproc2", "Uproc3"),
-        ("Uproc3", "Session3"),
-        ("Uproc4", "Uproc1"),
-    ]
-}
+# File uploader
+uploaded_file = st.sidebar.file_uploader("Upload JSON data", type=["json"])
 
-# Function to create the graph using Graphviz
-def create_graph(data):
-    dot = gv.Digraph(format="png")
+if uploaded_file is not None:
+    data = uploaded_file.getvalue().decode("utf-8")
+    json_data = json.loads(data)
+else:
+    st.warning('Please upload your JSON file.')
+    st.stop()
 
-    # Add nodes for sessions and uprocs
-    for session in data["sessions"]:
-        dot.node(session, shape="box", color="lightblue", style="filled")
-    for uproc in data["uprocs"]:
-        dot.node(uproc, shape="box", color="lightgreen", style="filled")
+def plot_network_graph():
+    # Create a directed graph (DiGraph) using Graphviz
+    G = pgv.AGraph(directed=True)
 
-    # Add edges for dependencies
-    for dependency in data["dependencies"]:
-        source, target = dependency
-        dot.edge(source, target)
+    # Add nodes and edges to the graph
+    for process_name, process_info in json_data.items():
+        G.add_node(process_name, shape="box")
+        for uprocs_info in process_info.get("uprocs", []):
+            uprocs_name = uprocs_info.get("name")
+            G.add_node(uprocs_name, shape="box")
+            G.add_edge(process_name, uprocs_name)
+            table_deps = uprocs_info.get("table_deps", {})
+            for table_dep, table_dep_content in table_deps.items():
+                if table_dep not in G.nodes():
+                    G.add_node(table_dep, shape="box", color="cyan", style="filled")
+                G.add_edge(uprocs_name, table_dep)
 
-    return dot
+    # Define graph properties
+    G.graph_attr["rankdir"] = "TB"  # Top to bottom layout
+    G.graph_attr["bgcolor"] = "#222222"
+    G.node_attr["fontcolor"] = "white"
+    G.node_attr["color"] = "white"
+
+    # Save the graph as an image
+    graph_path = "data_flow_graph.png"
+    G.draw(graph_path, format="png")
+
+    # Display the graph using Streamlit
+    st.header('Dépendance entre sessions-uprocs-table_deps')
+    st.image(graph_path, use_column_width=True)
 
 # Main Streamlit app code
-def main():
-    st.title("Network Graph Visualization with Graphviz on Streamlit")
-
-    st.sidebar.header("Graph Settings")
-    st.sidebar.write("Customize the graph as per your data.")
-
-    # Display graph settings (if any)
-
-    # Create and display the graph
-    graph = create_graph(data)
-    st.graphviz_chart(graph)
-
 if __name__ == "__main__":
-    main()
-
+    st.title("Orange Kenobi")
+    plot_network_graph()
