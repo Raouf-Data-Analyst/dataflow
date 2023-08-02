718ec6bd6b275d6d1f4a03054c0f8c96ea8fa1d5
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-08-02 15:08:02 +0200

    Update streamlit_bigQuery_app.py

--- streamlit_bigQuery_app.py
+++ streamlit_bigQuery_app.py
@@ -1,25 +1,28 @@
-import pandas_gbq
+# streamlit_app.py
+
 import streamlit as st
 from google.oauth2 import service_account
-import pandas as pd
+from google.cloud import bigquery
 
 # Create API client.
-credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-
-pandas_gbq.context.credentials = credentials
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
-def run_query():
-    query = "SELECT word FROM bigquery-public-data.samples.shakespeare LIMIT 10"
-    project_id = "modern-unison-394608"
-    df = pandas_gbq.read_gbq(query, project_id=project_id, dialect='standard',auth_local_webserver=False)
-    return df
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-df = run_query()
+rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
 
 # Print results.
 st.write("Some wise words from Shakespeare:")
-for row in df.itertuples():
-    st.write("✍️ " + row.word)
+for row in rows:
+    st.write("✍️ " + row['word'])
