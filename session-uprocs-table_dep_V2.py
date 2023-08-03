fee24e231592ee2a3a61dca64cab7f6e8c53dd74
Abderaouf_LAROUCI <99683777+Raouf-Data-Analyst@users.noreply.github.com>
2023-08-03 11:48:41 +0200

    Update session-uprocs-table_dep_V2.py

--- session-uprocs-table_dep_V2.py
+++ session-uprocs-table_dep_V2.py
@@ -63,7 +63,7 @@ def plot_network_graph():
     G.add_edges_from(edges)
 
     # Plot the interactived diagram using pyvis
-    nt = Network(height="750px", width="100%", bgcolor="#222222", font_color="white", directed=True, notebook=True, cdn=False)
+    nt = Network(height="750px", width="100%", bgcolor="#222222", font_color="white", directed=True, notebook=True)
     nt.show_buttons(filter_=['physics'])
     
     # Define colors for uprocs, input/output nodes, and nodes with table_deps
